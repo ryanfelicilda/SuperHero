fdafeebd8fc6820734e2ce36a7a2750e74eee1b7
ryanfelicilda <ryan.g.felicilda@gmil.com>
2019-09-30 09:12:26 -0500

    Currently adding SuperHero Controller, adding Create View into SuperHero View. Modifing GET ActionResult Create method, POST ActionResult Create method.

---
+++ SuperHero/SuperHero/Controllers/SuperHeroController.cs
@@ -0,0 +1,97 @@
+﻿using SuperHero.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperHero.Controllers
+{
+    public class SuperHeroController : Controller
+    {
+        ApplicationDbContext context;
+        public SuperHeroController()
+        {
+            context = new ApplicationDbContext();
+        }
+        // GET: SuperHero
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: SuperHero/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: SuperHero/Create
+        public ActionResult Create()
+        {
+            SuperHeroModels SuperHero = new SuperHeroModels();
+            return View(SuperHero);
+        }
+
+        // POST: SuperHero/Create
+        [HttpPost]
+        public ActionResult Create(SuperHeroModels SuperHero)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                context.People.Add(SuperHero);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SuperHero/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SuperHero/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SuperHero/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SuperHero/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
